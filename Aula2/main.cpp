cdc1a430b76031eb087581609870dd169b340cf0
mmpedro <marcopaulo.pedro@gmail.com>
2021-02-04 21:35:42 +0000

    Aula 2

---
+++ Aula2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declarar variáveis
+    int idade = 40;
+    double altura = 1.65;
+    string nome = "Marco Pedro";
+    char categoria = 'A';
+
+     cout  <<"idade:"<< idade << " Altura:" <<  altura << " Nome: " << nome<< " Categoria : "<< categoria;
+    return 0;
+}
